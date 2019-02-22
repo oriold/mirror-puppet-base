5ef6d0a373e6e482b69a64cb6c8c158f8a716698
Oriol Demaria <sysadmin@the-grid.xyz>
2019-02-22 14:12:04 +0000

    Demasiado ruido

--- manifests/init.pp
+++ manifests/init.pp
@@ -77,7 +77,6 @@ class base (
 
       package { [ $base_packages, $openbsd_packages ] :
         ensure          => installed,
-        install_options => '-v',
       }
 
       # Ports configuration
