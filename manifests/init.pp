415bd5c3524f470c566c5e23da10adc29905fe36
Oriol Demaria <sysadmin@the-grid.xyz>
2019-12-02 18:38:58 +0000

    Add path

--- manifests/init.pp
+++ manifests/init.pp
@@ -207,7 +207,7 @@ class base (
       }
 
       exec { 'create_aur_repo' :
-        command => 'repo-add /home/aur/custompkgs/custom.db.tar',
+        command => '/usr/bin/repo-add /home/aur/custompkgs/custom.db.tar',
         creates => '/home/aur/custompkgs/custom.db.tar',
         cwd     => '/home/aur/custompkgs',
         require => File['/home/aur/custompkgs'],
