42a2d2b5844116e11b040df86489345b0ac59e09
Oriol Demaria <sysadmin@the-grid.xyz>
2020-06-29 11:35:33 +0100

    Unattended upgrades

--- manifests/raspbian.pp
+++ manifests/raspbian.pp
@@ -5,8 +5,22 @@ class base::raspbian (
 
 ){
 
-  package { "unattended-upgrades":
+  package { [ "update-notifier-common", "unattended-upgrades" ] :
     ensure => installed,
   }
 
+  file { "/etc/apt/apt.conf.d/21auto-upgrades" :
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///modules/base/Debian/21auto-upgrades',
+  }
+
+  file { "/etc/apt/apt.conf.d/60unattendedupgrades" :
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///modules/base/Debian/60unattendedupgrades',
+  }
+  
 }
