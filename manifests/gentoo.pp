43c529e3718e0eccb1544e5170dff7d3eeaac3b4
Oriol Demaria <sysadmin@the-grid.xyz>
2020-07-08 14:16:10 +0100

    Add more stuff

--- manifests/gentoo.pp
+++ manifests/gentoo.pp
@@ -3,6 +3,11 @@ class base::gentoo (
 
 ) inherits base {
 
+  # Paquetes
+  package { [ $base_packages, $local_packages ] :
+    ensure => installed,
+  }
+
   package { 'cronie' :
     ensure => installed,
   }
@@ -11,6 +16,39 @@ class base::gentoo (
     ensure  => running,
     enable  => true,
     require => Package['cronie'],
+  }
+
+  package { 'openntpd' :
+    ensure => installed,
+  }
+
+  service { 'openntpd' :
+    ensure  => running,
+    enable  => true,
+    require => Package['openntpd'],
+  }
+
+  file { '/etc/ntpd.conf' :
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('base/ntpd.conf.erb'),
+    notify  => Service['openntpd'],
+  }
+
+  file { '/var/db/geoip' :
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+  }
+
+  # Vault
+  file { '/etc/profile.d/local-vault.sh' :
+    owner   => root,
+    group   => wheel,
+    mode    => '0644',
+    content => template('base/local-vault.sh.erb'),
   }
 
 }
