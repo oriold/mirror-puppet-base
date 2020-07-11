6c035f88e9830c923074e06583afde70fc617e47
Oriol Demaria <sysadmin@the-grid.xyz>
2020-07-11 20:33:36 +0100

    Basic stuff Alpine

--- manifests/alpine.pp
+++ manifests/alpine.pp
@@ -10,5 +10,45 @@ class base::alpine (
   package { [ $base_packages, $local_packages ] :
     ensure => installed,
   }
+
+  package { 'openntpd' :
+    ensure => installed,
+  }
+
+  service { 'openntpd' :
+    ensure  => running,
+    enable  => true,
+    require => Package['openntpd'],
+  }
+
+  file { '/etc/ntpd.conf' :
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('base/ntpd.conf.erb'),
+    notify  => Service['openntpd'],
+  }
+
+  file { '/etc/conf.d/loadkmap' :
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('base/Alpine/loadkmap.erb'),
+  }
+
+  file { '/var/db/geoip' :
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+  }
+
+  # Vault
+  file { '/etc/profile.d/local-vault.sh' :
+    owner   => root,
+    group   => wheel,
+    mode    => '0644',
+    content => template('base/local-vault.sh.erb'),
+  }
   
 }
