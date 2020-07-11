4bfd070880b2b9c66ba0873119bea8b15f43da23
Oriol Demaria <sysadmin@the-grid.xyz>
2020-07-11 12:23:05 +0100

    Add Alpine

---
+++ manifests/alpine.pp
@@ -0,0 +1,13 @@
+# For Alpine
+class base::alpine (
+
+) inherits base {
+
+  Package { provider => 'apk' }
+  Service { provider => 'openrc' }
+
+  # Paquetes
+  package { [ $base_packages, $local_packages ] :
+    ensure => installed,
+  }
+}
